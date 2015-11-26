ffa25d9e9ee701e5a40d3ecf2cc45a840782bf9e
Chathuranga Sandun <chathurangasandunkumara@gmail.com>
2015-11-27 02:20:11 +0530

    add operations of course and modules

---
+++ DeleteCourse.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Chathuranga-Pamba
+ * Date: 15/11/27
+ * Time: 2:18 AM
+ */
+
+require_once 'connect.php';
+if(!isset($_SESSION['isLoggedIn']) || $_SESSION['isLoggedIn']==false){
+    Redirect::to('index.php');
+}
+?>
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+    <title>Administrator</title>
+    <?php include 'headerScripts.php'?>
+</head>
+<body>
+<?php include 'navBar.php'?>
+<div class="container-fluid backgroundImg">
+    <?php include 'adminSidebar.php'?>
+    <div class="container col-lg-9">
+        <!--       write content in this division-->
+    </div>
+
+</div>
+<?php include 'footerScripts.php'?>
+<?php include 'footer.php'?>
+</body>
+</html>
